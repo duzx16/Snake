839b977ae349eb881bb94d66a8cad063c6645d5a
duzx <duzx16@mails.tsinghua.edu.cn>
2018-07-29 16:15:58 +0800

    修改了玩家2的操作方式

--- KeyboardController.java
+++ KeyboardController.java
@@ -9,28 +9,43 @@ public class KeyboardController extends KeyAdapter {
     }
 
     public void keyPressed(KeyEvent e) {
-        Dir dir = Dir.UP;
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-                dir = Dir.UP;
-                break;
-            case KeyEvent.VK_S:
-                dir = Dir.DOWN;
-                break;
-            case KeyEvent.VK_A:
-                dir = Dir.LEFT;
-                break;
-            case KeyEvent.VK_D:
-                dir = Dir.RIGHT;
-                break;
-        }
         if (_main.is_server_mode()) {
+            Dir dir = Dir.UP;
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_W:
+                    dir = Dir.UP;
+                    break;
+                case KeyEvent.VK_S:
+                    dir = Dir.DOWN;
+                    break;
+                case KeyEvent.VK_A:
+                    dir = Dir.LEFT;
+                    break;
+                case KeyEvent.VK_D:
+                    dir = Dir.RIGHT;
+                    break;
+            }
             _main.data.lock.writeLock().lock();
             if (!_main.data.snakes[0].body.elementAt(1).equalTo(GameLogic._dir_pos[dir.ordinal()])) {
                 _main.data.dirs[0] = dir;
             }
             _main.data.lock.writeLock().unlock();
         } else {
+            Dir dir = Dir.UP;
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    dir = Dir.UP;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    dir = Dir.DOWN;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    dir = Dir.LEFT;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    dir = Dir.RIGHT;
+                    break;
+            }
             _main.clientListener.sendDirData(dir);
         }
     }
