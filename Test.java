2b8403ad1710d49a2698b79a9de0f3d6ccac7ce1
duzx <duzx16@mails.tsinghua.edu.cn>
2018-07-29 16:13:09 +0800

    在事件派发队列中创建程序

--- Test.java
+++ Test.java
@@ -3,11 +3,8 @@ import java.awt.*;
 
 public class Test {
     public static void main(String[] args) {
-        GameMain gameMain = new GameMain();
-//        GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
-//        String[] fontNames = e.getAvailableFontFamilyNames();
-//        for (String fontName : fontNames) {
-//            System.out.println(fontName);
-//        }
+        SwingUtilities.invokeLater(() -> {
+            GameMain app = new GameMain();
+        });
     }
 }
