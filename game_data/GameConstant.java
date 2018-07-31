f40289f9b78e7964da0222fdb0eb6ffb803a7309
duzx <duzx16@mails.tsinghua.edu.cn>
2018-07-31 10:18:33 +0800

    增加两个按钮图片

---
+++ game_data/GameConstant.java
@@ -0,0 +1,15 @@
+package game_data;
+
+public class GameConstant {
+    // The game setting
+    public static final int map_width = 30, map_height = 30;
+    public static final int timer_interval = 20;
+    public static final int min_game_interval = 5;
+    public static final int food_wait = 2000;
+    public static final int hole_wait = 2000;
+    // The connect setting
+    public static final int client_patience = 2000;
+    // The animation setting
+    public static final int death_flash_delay = 2000;
+    public static final int plus_one_interval = 3;
+}
